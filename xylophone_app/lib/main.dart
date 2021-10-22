fd1e2098863e31238a41d9b7269a879f5186723f
malikrasaq <malikrasaq@gmail.com>
2021-10-22 16:54:58 +0100

    Update UI

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -6,20 +6,84 @@ void main() => runApp(const XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
 
+  void playSound(int soundNumber) {
+       final player = AudioCache();
+       player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.green),
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: const Text('Click me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.red),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                    child: const Text('one'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.white),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: const Text('two'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.green),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child:const Text('three'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.teal),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                    child: const Text('four'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                    child: const Text('five'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                    child: const Text('six'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.brown),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: const Text('seven'),
+                ),
+              ),
+            ],
           ),
         ),
       ),
